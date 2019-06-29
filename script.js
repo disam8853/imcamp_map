237665b73a2cdbb7e916758b6c4185263d30fd75
eethan1 <call132call132@gmail.com>
2019-06-29 16:41:05 +0800

    egg;schedule page;team point;

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
-$('#btn1').css('top','5vmax');
-$('#btn1').css('left','50vmax');
+var tops= [35, 43, 55, 31, 29, 28, 22, 24, 30, 52];
+var lefts=[14, 13,  7, 33, 47, 52, 66, 76, 71, 83];
 
-var show = [false,false];
-var currentShow = -1;
-function popup(index) {
-    console.log(index, show[index]);
-    currentShow = index;
-    if(show[index]) {
-        $('#team'+index).hide();
-        $('#mapimg').show();
-    } else {
-        $('#team'+index).show();
-        $('#mapimg').hide();
-    }
-    show[index] = !show[index];
+var tri = "V3pJeUxESTNMREkzTERRd0xETTNMRFF3TERNekxETXpMaklzTXpjc016Y3VNMTA9";
+var vago = "V3pZc09DdzBMRFEyTERRNExEUTRMalVzTnpFc056UXNOekF1Tnl3M05GMD0=";
+
+var defaultShow = '#map';
+var currentShow = defaultShow;
+function showTeam(i) {
+    $('.teamimg').attr('src', 'img/teams/Team'+i+'/bg.png');
+    showup('#team');
+}
+
+function showup(e) {
+    $(currentShow).hide();
+    $(e).css('display', 'block');
+    $('#btnback').show();
+    currentShow = e;
 }
 
 function goback() {
-    if(currentShow != -1) {
-        $("#team"+currentShow).hide();
-        $('#mapimg').show();
-        show[currentShow] = false;
+    if(currentShow != defaultShow) {
+        $(currentShow).hide();
+        $(defaultShow).show();
+        $('#btnback').hide();
     }
-    currentShow = -1;
+    currentShow = defaultShow;
 }
-for(let i=1;i<=1;++i) {
-    $("#btn"+i).on("click",()=>{popup(i)});
+for(let i=1;i<=10;++i) {
+    $('#btn'+i).css('top', tops[i-1]+'vh');
+    $('#btn'+i).css('left', lefts[i-1]+'vw');
+    $('#btn'+i).on('click',()=>{showTeam(i)});
 }
+var talk = 0;
+function tell() {
+    if(talk==0)
+        alert("Where to go? tri vago");
+    else if(talk>=1 && talk <=3)
+        alert("We don't talk anymore~");
+    else if(talk ==4) 
+        alert("like we used to~ do");
+    else 
+        alert("Why not take a look at 丁5");
+    talk = talk +1;
+}
+$(window).click(function() {
+    // this.window.moveTo(0,1);
+    console.log('jizz');
+    // document.body.requestFullscreen();
+});
