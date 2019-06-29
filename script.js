95f9b5c60d4046cb955f8481de9fa716f9023d1c
eethan1 <call132call132@gmail.com>
2019-06-29 17:49:05 +0800

    story;One Piece

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var tops= [35, 43, 55, 31, 29, 28, 22, 24, 30, 52];
 var lefts=[14, 13,  7, 33, 47, 52, 66, 76, 71, 83];
 
-var tri = "V3pJeUxESTNMREkzTERRd0xETTNMRFF3TERNekxETXpMaklzTXpjc016Y3VNMTA9";
-var vago = "V3pZc09DdzBMRFEyTERRNExEUTRMalVzTnpFc056UXNOekF1Tnl3M05GMD0=";
+var One = "V3pJeUxESTNMREkzTERRd0xETTNMRFF3TERNekxETXpMaklzTXpjc016Y3VNMTA9";
+var Piece = "V3pZc09DdzBMRFEyTERRNExEUTRMalVzTnpFc056UXNOekF1Tnl3M05GMD0=";
 
 var defaultShow = '#map';
 var currentShow = defaultShow;
@@ -10,7 +10,10 @@ function showTeam(i) {
     $('.teamimg').attr('src', 'img/teams/Team'+i+'/bg.png');
     showup('#team');
 }
-
+function showIframe(id,src) {
+    // $(id).attr('src', src);
+    showup('#'+id+'Con');
+}
 function showup(e) {
     $(currentShow).hide();
     $(e).css('display', 'block');
@@ -34,17 +37,15 @@ for(let i=1;i<=10;++i) {
 var talk = 0;
 function tell() {
     if(talk==0)
-        alert("Where to go? tri vago");
+        alert("Does One Piece really exist?");
     else if(talk>=1 && talk <=3)
         alert("We don't talk anymore~");
     else if(talk ==4) 
         alert("like we used to~ do");
     else 
-        alert("Why not take a look at 丁5");
+        alert("Why not take a look at 丁@v@5ㄈ尺1p+?");
     talk = talk +1;
 }
 $(window).click(function() {
-    // this.window.moveTo(0,1);
     console.log('jizz');
-    // document.body.requestFullscreen();
 });
